24de371da42b899c064e0e73396494700f710fe9
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-04 21:01:54 -0300

    first commit

---
+++ src/Todos.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Todos = ({ todos }) => {
+  const todoList = todos.length ? (
+    todos.map((todo) => {
+      return (
+        <div key={todo.id} className="collection-item">
+          <span>{todo.content}</span>
+        </div>
+      );
+    })
+  ) : (
+    <p className="center"> You have no todo's left, yay1</p>
+  );
+  return <div className="todos collection"></div>;
+};
+
+export default Todos;
