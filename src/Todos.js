a2a93795ce5ba3e2522f80e87a3a1cb39f4676b1
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-05 21:00:26 -0300

    clicking on task, deletes it

--- src/Todos.js
+++ src/Todos.js
@@ -1,42 +1,24 @@
 import React from "react";
 
-const Todos = ({ todos }) => {
-  const todoList = todos.length ? (
-    todos.map((todo) => {
-      return (
-        <div key={todo.id} className="collection-item">
-          <span>{todo.content}</span>
-        </div>
-      );
-    })
-  ) : (
-    <p className="center"> You have no todo's left, yay1</p>
-  );
-  return <div className="todos collection"></div>;
-};
-
-export default Todos;
-import React from "react";
-
 const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
       return (
         <div key={todo.id} className="collection-item">
           <span
-            >{todo.content}</span>
-          <button
-          onClick={()=> deleteTodo(todo.id)}
-          >X</button>
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          >
+            {todo.content}
+          </span>
         </div>
       );
     })
   ) : (
-    <p className="center"> You have no todo's left, yay1</p>
+    <p className="center"> You have no todo's left, yay!</p>
   );
-  return <div className="todos collection">
-    {todoList}
-  </div>;
+  return <div className="todos collection">{todoList}</div>;
 };
 
 export default Todos;
