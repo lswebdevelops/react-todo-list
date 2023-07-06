6ef0eff181abc177521f37ad2cc820db91e79449
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-05 21:21:11 -0300

    adding an input item to add new items to array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Todos from "./Todos";
+import AddTodo from "./AddTodo";
 
 class App extends Component {
   state = {
@@ -10,20 +11,31 @@ class App extends Component {
   };
 
   deleteTodo = (id) => {
-    const todos = this.state.todos.filter(todo => {
-        return todo.id !== id
-     })
-        this.setState({
-            todos: todos
-        })
-   
-};
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos
+    });
+  };
+
+  addTodo = (todo) => {
+    // for now math.random will do it
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo]
+    this.setState({
+        todos
+    })
+
+  }
+
 
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
